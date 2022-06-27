24ec5af80bca49b53c084d07b3a857a9691113ae
eatsalonedev <joshuacharley87@gmail.com>
2022-06-27 22:51:24 +0000

    wip

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const routes = require('./routes/mark')
+const cors = require("cors");
+
 
 
 const app = express();
 
+app.use(
+    cors()
+  );
+  
 app.use(express.json());
 
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-
-app.use(cors(corsOptions)) // Use this after the variable declaration
-
-//add middleware here
 app.use('/', routes);
 
 
